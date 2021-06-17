80060c9c8cc2556206cdcfab089b98a2925aedf4
aardaipek <ardaipek66@gmail.com>
2021-06-17 22:01:07 +0300

    updated

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,19 +5,26 @@ import logs from '../config/log';
 const NAMESPACE = "User Controller";
 
 const getUser = (req: Request, res: Response, next: NextFunction) => {
-    logs.info(NAMESPACE, `User get route called`);
+    try {
+        logs.info(NAMESPACE, `User get route called`);
 
-    return res.status(200).json({
-        id: req.params.id
-    })
+        return res.status(200).json({
+            id: req.params.id
+        })
+    } catch (err) {
+        logs.error(NAMESPACE, err);
+        return res.status(409).send(err.message);
+    }
+
 };
 
 const userCreate = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        logs.info(NAMESPACE, `User create route called`);
         const user = await createUser(req.body);
         return res.send(user);
     } catch (err) {
-        logs.warn(NAMESPACE, err);
+        logs.error(NAMESPACE, err);
         return res.status(409).send(err.message);
     }
 };
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import config from "config";
 
-
 export interface UserDocument extends mongoose.Document {
   email: string;
   name: string;
