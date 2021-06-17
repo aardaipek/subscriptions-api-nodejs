74204bb23b9f599a8f0298bba759aba0769c91c7
aardaipek <ardaipek66@gmail.com>
2021-06-17 22:33:47 +0300

    environment variables added for firebase service

--- dist/config/config.js
+++ dist/config/config.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
-const SERVER_PORT = process.env.SERVER_PORT || 1337;
+const SERVER_PORT = process.env.SERVER_PORT;
 const SERVER = {
     hostname: SERVER_HOSTNAME,
     port: SERVER_PORT
